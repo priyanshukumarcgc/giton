4927036b882d16557190d7f647bdbc1f3f0149c8
priyanshu kumar <kumarpriyanshu007.pk@gmail.com>
2020-06-29 10:03:33 +0530

    i am very happy this is second git file which i modified

--- all_checks.py
+++ all_checks.py
@@ -1,3 +1,10 @@
+import os
+
+def check_rebot():
+    """returns true if computer has a pending rebot request ."""
+    return os.path.exists("run/rebot-required")
+
+    
 def main():
     pass
 main()
