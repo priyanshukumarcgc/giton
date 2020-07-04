5654f4c33e57283dc8c282db379218a26584f5e5
priyanshu kumar <kumarpriyanshu007.pk@gmail.com>
2020-07-04 11:29:26 +0530

    call check_rebot from main and return 1 if error exit

--- all_checks.py
+++ all_checks.py
@@ -1,10 +1,12 @@
 import os
-
+import sys
 def check_rebot():
     """returns true if computer has a pending rebot request ."""
     return os.path.exists("run/rebot-required")
 
     
 def main():
-    pass
+    if check_rebot():
+        print("pending rebot.")
+        sys.exit(1)
 main()
