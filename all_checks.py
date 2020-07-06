aa729afe1467757064b784d6ccfce410cb1f9ea5
priyanshu kumar <kumarpriyanshu007.pk@gmail.com>
2020-07-06 10:06:32 +0530

    add disk_full function to commit

--- all_checks.py
+++ all_checks.py
@@ -9,6 +9,9 @@ def main():
     if check_rebot():
         print("pending rebot.")
         sys.exit(1)
-        print("every thing is okay.")
-        sys.exit(0)
+    if disk_full():
+        print("disk full.")
+        sys.exit(1)
+    print("every thing is okay.")
+    sys.exit(0)
 main()
