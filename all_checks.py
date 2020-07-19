3a9d88eafdfb62de35b22d6887e64520696ef8d7
priyanshu kumar <kumarpriyanshu007.pk@gmail.com>
2020-07-19 15:38:03 +0530

    new cooment modified

--- all_checks.py
+++ all_checks.py
@@ -4,7 +4,7 @@ def check_rebot():
     """returns true if computer has a pending rebot request ."""
     return os.path.exists("run/rebot-required")
 
-    
+    #hii this check
 def main():
     if check_rebot():
         print("pending rebot.")
