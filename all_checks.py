7c1c3125767f1ffdeab080b96c852d02b432668f
priyanshu kumar <kumarpriyanshu007.pk@gmail.com>
2020-07-21 21:40:23 +0530

    a message for print1 func is commit

--- all_checks.py
+++ all_checks.py
@@ -3,10 +3,10 @@ import sys
 def check_rebot():
     """returns true if computer has a pending rebot request ."""
     return os.path.exists("run/rebot-required")
-
+#this is just for print pk name of author
 def print1():
     print("hii this is pk")
-    
+
 def main():
     print1()
     if check_rebot():
